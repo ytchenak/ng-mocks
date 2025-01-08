22d03da3d612636f781821c963ff545919b9cee4
Yakov Tchenak <yakov_tchenak@bmc.com>
2025-01-08 12:13:35 +0200

    Revert decorate-declaration.ts
    Signed-off-by: Yakov Tchenak <yakov_tchenak@bmc.com>

--- libs/ng-mocks/src/lib/mock/decorate-declaration.ts
+++ libs/ng-mocks/src/lib/mock/decorate-declaration.ts
@@ -46,8 +46,6 @@ export default <T extends Component & Directive>(
     },
   params: T,
 ): Component & Directive => {
-
-  const standalone = ((source as any).ɵcmp || (source as any).ɵdir || (source as any).ɵpipe)?.standalone;
   const hasResolver = ngMocksUniverse.config.has('mockNgDefResolver');
   if (!hasResolver) {
     ngMocksUniverse.config.set('mockNgDefResolver', new CoreDefStack());
@@ -63,13 +61,9 @@ export default <T extends Component & Directive>(
   if (meta.selector !== undefined) {
     options.selector = meta.selector;
   }
-  
   if (meta.standalone !== undefined) {
     options.standalone = meta.standalone;
-  } else if (standalone !== undefined) {
-    options.standalone = standalone;
   }
-
 
   if (meta.standalone && meta.imports) {
     const [, { imports }] = mockNgDef({ imports: meta.imports, skipExports: true });
